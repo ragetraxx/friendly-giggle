6da446a15f68c1334941c6c676c2ffed37439fb0
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-03-30 05:37:47 +0800

    Update restream.py

--- restream.py
+++ restream.py
@@ -4,7 +4,7 @@ import os
 import time
 
 RTMP_URL = os.getenv("RTMP_URL")  # RTMP Server URL
-VIDEO_URL = "http://fl6.moveonjoy.com/PPV_1/index.m3u8"  # Video source
+VIDEO_URL = "http://fl6.moveonjoy.com/PPV_2/index.m3u8"  # Video source
 OVERLAY_IMAGE = "overlay.png"  # Overlay image (optional)
 OVERLAY_TEXT = "UFC Fight Night: Moreno vs. Erceg"  # Overlay text
 
