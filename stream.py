86542a9e0b6717277b1a216d4c44329b0ded0b17
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-06-23 07:16:26 +0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -57,7 +57,7 @@ def build_ffmpeg_command(url, title):
         f"[0:v]scale=854:480:flags=lanczos,unsharp=5:5:0.8:5:5:0.0[v];"
         f"[1:v]scale=854:480[ol];"
         f"[v][ol]overlay=0:0[vo];"
-        f"[vo]drawtext=fontfile='{FONT_PATH}':text='{text}':fontcolor=white:fontsize=20:x=30:y=30",
+        f"[vo]drawtext=fontfile='{FONT_PATH}':text='{text}':fontcolor=white:fontsize=13:x=30:y=30",
         "-r", "29.97003",
         "-c:v", "libx264",
         "-profile:v", "high",
