4539e101acf7d4a150770514cc7f30c62c43f33a
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-08-15 05:30:56 +0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -35,13 +35,11 @@ def escape_drawtext(text):
 def build_ffmpeg_command(url, title):
     text = escape_drawtext(title)
 
-    input_options = []
-    if ".m3u8" in url or "streamsvr" in url:
-        print(f"🔐 Spoofing headers for {url}")
-        input_options = [
-            "-user_agent", "Mozilla/5.0",
-            "-headers", "Referer: https://hollymoviehd.cc\r\n"
-        ]
+    # NASA+ headers
+    input_options = [
+        "-user_agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "-headers", "Referer: https://plus.nasa.gov/\r\nOrigin: https://plus.nasa.gov\r\n"
+    ]
 
     return [
         "ffmpeg",
